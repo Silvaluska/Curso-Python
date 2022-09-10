ed9953b29d4377f9e340c3eb94bebf30817fd5ff
Lucas Silva <lucas030894@gmail.com>
2022-09-10 15:03:36 -0300

    Create Aula 07 - Desafio 13.py

---
+++ Exercicios/Aula 07 - Desafio 13.py
@@ -0,0 +1,5 @@
+'''
+Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+'''
+s = float(input('Digite o valor do seu salário: '))
+print('O seu novo salário com aumento de 15% é {:.2f}'.format(s*1.15))
