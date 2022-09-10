b3d4b01fc05d738d38b778da8c7a51409820baa7
Lucas Silva <lucas030894@gmail.com>
2022-09-10 14:58:53 -0300

    Create Aula 07 - Desafio 9.py

---
+++ Exercicios/Aula 07 - Desafio 9.py
@@ -0,0 +1,16 @@
+'''
+Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+'''
+n = int(input('Digite um número: '))
+print('-'*11)
+print('{} x  1 = {}'.format(n, n*1))
+print('{} x  2 = {}'.format(n, n*2))
+print('{} x  3 = {}'.format(n, n*3))
+print('{} x  4 = {}'.format(n, n*4))
+print('{} x  5 = {}'.format(n, n*5))
+print('{} x  6 = {}'.format(n, n*6))
+print('{} x  7 = {}'.format(n, n*7))
+print('{} x  8 = {}'.format(n, n*8))
+print('{} x  9 = {}'.format(n, n*9))
+print('{} x 10 = {}'.format(n, n*10))
+print('-'*11)
