b42d30749fb3754cb69ba28550bf199243ca28bc
Lucas Silva <lucas030894@gmail.com>
2022-09-10 14:56:04 -0300

    Create Aula 07 - Desafio 7.py

---
+++ Exercicios/Aula 07 - Desafio 7.py
@@ -0,0 +1,6 @@
+'''
+Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+'''
+n1 = float(input("Digite o valor da primeira nota: "))
+n2 = float(input("Digite o valor da Segunda nota: "))
+print("A média das suas notas é {}".format((n1+n2)/2))
