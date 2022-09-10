2d622d0c8aed594aa7811721660d6c2236be8743
Lucas Silva <lucas030894@gmail.com>
2022-09-10 15:02:22 -0300

    Create Aula 07 - Desafio 12.py

---
+++ Exercicios/Aula 07 - Desafio 12.py
@@ -0,0 +1,5 @@
+'''
+Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+'''
+p = float(input('Digite o preço: '))
+print('O preço com desconto de 5% é {:.2f}'.format(p*0.95))
