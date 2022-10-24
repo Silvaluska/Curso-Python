aec0d4647f27ff8ece9cbe430406f45e0bb3aa02
Lucas Silva <lucas030894@gmail.com>
2022-10-24 15:08:21 -0300

    Update OO2.py

--- CursoOO/OO2.py
+++ CursoOO/OO2.py
@@ -1,12 +1,35 @@
-class filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome
-        self.__ano = ano
-        self.__duracao = duracao
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome
+        self._ano = ano
+        self._likes = 0
 
     @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+    
+    @property
     def nome(self):
-        return self.__nome.title()
+        return self._nome.title()
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self._duracao = duracao
+
+class serie(Programa):
+    def __init__(self, nome, ano, temporada):
+        super().__init__(nome, ano)
+        self._temporada = temporada
+
 
 vingadores = filme('meu filme', 2018, 160)
 print(vingadores.nome)
